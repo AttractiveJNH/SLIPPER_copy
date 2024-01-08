2893311249fe257b5b8f560dbac8c95579843bee
주남희 <141652584+AttractiveJNH@users.noreply.github.com>
2024-01-08 11:20:58 +0900

    StartingPoint

---
+++ src/test/java/com/example/Slipper/SlipperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Slipper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
